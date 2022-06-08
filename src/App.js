9457a67dcbd71319f55acbdff085fd1ad40232ad
Edmundo Guerrero <edmundo.gt.esc@gmail.com>
2022-06-08 12:39:06 -0600

    Maquetado todos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+
+const todos = [
+  { text: "Cortar Cebolla", completed: true },
+  { text: "Tomar el curso", completed: false },
+  { text: "Llorar con la llorona", completed: false },
+];
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        {todos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+            />
+          );
+        })}
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
