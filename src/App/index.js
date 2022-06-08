707240e76ecf0f7f0175418d0148f6959b843bc5
Edmundo Guerrero <edmundo.gt.esc@gmail.com>
2022-06-08 14:04:00 -0600

    Persistencia de datos en localStorage - Guardar los datos y recuperarlos

--- src/App/index.js
+++ src/App/index.js
@@ -2,15 +2,26 @@ import React from "react";
 // import "./App.css";
 import { AppUI } from "./AppUI";
 
-const defaultTodos = [
-  { text: "Cortar Cebolla", completed: true },
-  { text: "Tomar el curso", completed: false },
-  { text: "Llorar con la llorona", completed: false },
-  { text: "Llorar con la llorona 2", completed: true },
-];
+// const defaultTodos = [
+//   { text: "Cortar Cebolla", completed: true },
+//   { text: "Tomar el curso", completed: false },
+//   { text: "Llorar con la llorona", completed: false },
+//   { text: "Llorar con la llorona 2", completed: true },
+// ];
 
 function App(props) {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem("TODOS_V1");
+
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem("TODOS_V1", JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [param, setParam] = React.useState("");
   const completedTodos = todos.filter((todo) => todo.completed).length;
   const totalTodos = todos.length;
@@ -28,18 +39,24 @@ function App(props) {
     });
   }
 
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem("TODOS_V1", stringifiedTodos);
+    setTodos(newTodos);
+  };
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
